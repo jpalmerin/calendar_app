a036e0c528f488c059cb1d3cfd88f76f5de41ea9
Josue Palmerin <jpalmerin@nueone.com>
2013-06-14 10:49:03 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 CalendarApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
  #get "users/new"
 
   root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/signup',  to: 'users#new'
 
